a8c6bf8beeb203c4ebbfeab9c3505ef80d12d183
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-15 12:16:16 -0700

    Update certificateconfig.js

--- certificateconfig.js
+++ certificateconfig.js
@@ -3,11 +3,8 @@ export const AUTH_CONFIG = {
   // Editable secret names (without exposing values)
   SECRET_NAMES: {
     ALLOWED_KEYS: "ALLOWED_KEYS",  // Name of the secret containing allowed keys
-    ACCESS_PASSWORD: "GRUMPYHORSE404"  // Name of the password secret
+    ACCESS_PASSWORD: "test1"  // Name of the password secret
   },
   
-  // Fixed repo details
-  REPO_OWNER: "ZBZFirst",
-  REPO_NAME: "LockBox",
   AUTH_FILE: "testauth.csv"
 };
