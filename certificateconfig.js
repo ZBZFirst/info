2254da131ade48965f8f9ae4ea78a435eea8bd4d
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-15 13:35:10 -0700

    Update certificateconfig.js

--- certificateconfig.js
+++ certificateconfig.js
@@ -66,3 +66,10 @@ export const CERTIFICATE_TEMPLATE = {
     }
   ]
 };
+
+export const AUTH_CONFIG = {
+  // Editable secret names (without exposing values)
+  SECRET_NAMES: {
+    ALLOWED_KEYS: "ALLOWED_KEYS",  // Name of the secret containing allowed keys
+    ACCESS_PASSWORD: "test1"  // Name of the password secret
+  },
