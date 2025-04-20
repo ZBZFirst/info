fff0c1d4d2257ff575b47a966430a0c639533469
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-19 21:18:19 -0700

    Update certificateconfig.js

--- certificateconfig.js
+++ certificateconfig.js
@@ -27,7 +27,7 @@ export const CERTIFICATE_TEMPLATE = {
       <title>{{title}}</title>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="certificate.css">
+      <link rel="stylesheet" href="_css/certificate.css">
       <style>
         /* Inline critical CSS for print reliability */
         @page {
