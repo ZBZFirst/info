4971071f7603ac3a14c78f1b4448964c9f3230d3
ZBZFirst <132862961+ZBZFirst@users.noreply.github.com>
2025-04-25 21:27:58 -0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -182,6 +182,12 @@ async function initDashboard() {
   }
 }
 
+// Helper Functions
+function getColor(index) {
+  const colors = ["#4e79a7", "#f28e2b", "#e15759", "#76b7b2", "#59a14f", "#edc948", "#b07aa1", "#ff9da7"];
+  return colors[index % colors.length];
+}
+
 function toggleFullscreen() {
   const isFullscreen = chartContainer.dataset.fullscreen === "true";
   chartContainer.dataset.fullscreen = !isFullscreen;
